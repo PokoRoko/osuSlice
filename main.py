301bcd1ebb50f4c369e798bc9d35c90a24e8cb55
PokoRoko <peuc386@gmail.com>
2020-07-03 11:17:18 +0300

    Added osuSlice.exe and discription

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ from edit_mp3 import edit_train_mp3
 from pydub import AudioSegment
 from search_train_files import address_list
 
-# Settingsьфш
+# Settings
 # Имя нового mp3 на выходе
 name_new_mp3 = "mytrain.mp3"  #def="mytrain.mp3"
 # Количество повторов
-num_repeats = 10
+num_repeats = 15
 # Время разрыва между повторами
 break_time = 7000
 
